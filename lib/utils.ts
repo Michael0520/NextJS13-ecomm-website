3bc30ebf107e1869ae2b6de55a94623a2179ddc8
Michael0520 <po2319987@gmail.com>
2023-09-09 03:35:38 +0800

    feat: 🎸 add utils func about isMacOs

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,10 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+export function isMacOs() {
+  if (typeof window === "undefined") return false
+
+  return window.navigator.userAgent.includes("Mac")
+}
