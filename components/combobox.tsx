246261821f00fc75882bc3dd604666612ae795e0
Michael0520 <po2319987@gmail.com>
2023-09-09 04:10:10 +0800

    feat: 🎸 add combobox component in site-header

---
+++ components/combobox.tsx
@@ -0,0 +1,112 @@
+"use client"
+
+import * as React from "react"
+import { useRouter } from "next/navigation"
+
+// import { type Product } from "@/db/schema"
+
+import { cn, isMacOs } from "@/lib/utils"
+// import { useDebounce } from "@/hooks/use-debounce"
+import { Button } from "@/components/ui/button"
+import {
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command"
+import { Skeleton } from "@/components/ui/skeleton"
+import { Icons } from "@/components/icons"
+
+// import { filterProductsAction } from "@/app/_actions/product"
+
+export function Combobox() {
+  const router = useRouter()
+  const [isOpen, setIsOpen] = React.useState(false)
+  const [query, setQuery] = React.useState("")
+  // const debouncedQuery = useDebounce(query, 300)
+  //   const [data, setData] = React.useState<
+  //     | {
+  //         category: Product["category"]
+  //         products: Pick<Product, "id" | "name" | "category">[]
+  //       }[]
+  //     | null
+  //   >(null)
+  const [isPending, startTransition] = React.useTransition()
+  const [isMac, setIsMac] = React.useState(false)
+
+  // TODO: usaDebounce
+  //   React.useEffect(() => {
+  //     if (debouncedQuery.length === 0) setData(null)
+
+  //     if (debouncedQuery.length > 0) {
+  //       startTransition(async () => {
+  //         const data = await filterProductsAction(debouncedQuery)
+  //         setData(data)
+  //       })
+  //     }
+  //   }, [debouncedQuery])
+
+  React.useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault()
+        setIsOpen((isOpen) => !isOpen)
+      }
+    }
+    window.addEventListener("keydown", handleKeyDown)
+    return () => window.removeEventListener("keydown", handleKeyDown)
+  }, [])
+
+  // 選擇後會執行對應的業務邏輯
+  const handleSelect = React.useCallback((callback: () => unknown) => {
+    setIsOpen(false)
+    callback()
+  }, [])
+
+  React.useEffect(() => {
+    if (!isOpen) {
+      setQuery("")
+    }
+  }, [isOpen])
+
+  React.useEffect(() => {
+    setIsMac(isMacOs())
+  }, [])
+
+  return (
+    <>
+      <Button
+        variant="outline"
+        className="relative h-9 w-9 p-0 xl:h-10 xl:w-60 xl:justify-start xl:px-3 xl:py-2"
+        onClick={() => setIsOpen(true)}
+      >
+        <Icons.search className="h-4 w-4 xl:mr-2" aria-hidden="true" />
+        <span className="hidden xl:inline-flex">Search products...</span>
+        <span className="sr-only">Search products</span>
+        <kbd className="pointer-events-none absolute right-1.5 top-2 hidden h-6 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium opacity-100 xl:flex">
+          <abbr title={isMac ? "Command" : "Control"}>
+            {isMac ? "⌘" : "Ctrl+"}
+          </abbr>
+          K
+        </kbd>
+      </Button>
+      <CommandDialog position="top" open={isOpen} onOpenChange={setIsOpen}>
+        <CommandInput
+          placeholder="Search products..."
+          value={query}
+          onValueChange={setQuery}
+        />
+        <CommandList>
+          <CommandEmpty
+            className={cn(isPending ? "hidden" : "py-6 text-center text-sm")}
+          >
+            No products found.
+          </CommandEmpty>
+          {/* TODO:Product List */}
+        </CommandList>
+      </CommandDialog>
+    </>
+  )
+}
