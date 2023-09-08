457c848ab095c824029ab5d4f2a30047399a0f72
Michael0520 <po2319987@gmail.com>
2023-09-09 04:41:59 +0800

    feat: 🎸 add useMemo to control breadcrumb component

--- app/(lobby)/components/Breadcrumb.tsx
+++ app/(lobby)/components/Breadcrumb.tsx
@@ -1,47 +1,46 @@
-import { FC } from "react"
+import { FC, useMemo } from "react"
 import { ChevronRightIcon } from "@radix-ui/react-icons"
 
-interface BreadcrumbItem {
+type BreadcrumbItem = {
   name: string
   link: string
   current?: boolean
 }
 
-interface BreadcrumbProps {
+type BreadcrumbProps = {
   items: BreadcrumbItem[]
 }
 
 const Breadcrumb: FC<BreadcrumbProps> = ({ items }) => {
-  const processedItems = items.map((item, index) => {
-    const shouldShowIcon = index > 0
-    const isCurrent = item.current
+  const renderedItems = useMemo(() => {
+    return items.map((item, index) => {
+      const isIconVisible = index > 0
+      const content = item.current ? (
+        <span className="text-sm font-medium text-gray-500 dark:text-gray-400">
+          {item.name}
+        </span>
+      ) : (
+        <a
+          href={item.link}
+          className="text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white"
+        >
+          {item.name}
+        </a>
+      )
 
-    const icon = shouldShowIcon ? <ChevronRightIcon className="mx-1" /> : null
-    const content = isCurrent ? (
-      <span className="text-sm font-medium text-gray-500 dark:text-gray-400">
-        {item.name}
-      </span>
-    ) : (
-      <a
-        href={item.link}
-        className="text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white"
-      >
-        {item.name}
-      </a>
-    )
-
-    return { icon, content }
-  })
+      return (
+        <li className="inline-flex items-center" key={index}>
+          {isIconVisible && <ChevronRightIcon className="mx-1" />}
+          {content}
+        </li>
+      )
+    })
+  }, [items])
 
   return (
     <nav className="container flex h-8" aria-label="Breadcrumb">
       <ol className="inline-flex items-center space-x-1 md:space-x-3">
-        {processedItems.map((item, index) => (
-          <li className="inline-flex items-center" key={index}>
-            {item.icon}
-            {item.content}
-          </li>
-        ))}
+        {renderedItems}
       </ol>
     </nav>
   )
