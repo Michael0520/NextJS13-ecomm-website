9599f2e7d2e381c3846d023593cb12d1dbdbd2b8
Michael0520 <po2319987@gmail.com>
2023-09-09 04:35:26 +0800

    feat: 🎸 Update page UI structure

--- app/(lobby)/page.tsx
+++ app/(lobby)/page.tsx
@@ -1,5 +1,19 @@
+import Breadcrumb from "./components/Breadcrumb"
+
 const IndexPage = () => {
-  return <div>123</div>
+  return (
+    <div>
+      {/* Breadcrumb - Block */}
+      <Breadcrumb />
+      {/* Banner - Block */}
+
+      {/* Page Title - Block */}
+      {/* Page Description - Group - three column*/}
+      {/* HashTag - Block - font-bold */}
+      {/* Button Group - inline - About 查看類似商品、團購訂單 */}
+      {/* Outlet about Left Sidebar , Right Product List */}
+    </div>
+  )
 }
 
 export default IndexPage
