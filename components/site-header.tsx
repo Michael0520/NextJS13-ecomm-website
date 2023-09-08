0dc8bef458e370b5d0b2d1e73f5a281ab09d77f6
Michael0520 <po2319987@gmail.com>
2023-09-09 04:35:54 +0800

    feat: 🎸 add combobox in site-header

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -6,13 +6,17 @@ import { Icons } from "@/components/icons"
 import { MainNav } from "@/components/main-nav"
 import { ThemeToggle } from "@/components/theme-toggle"
 
+import { Combobox } from "./combobox"
+
 export function SiteHeader() {
   return (
-    <header className="bg-background sticky top-0 z-40 w-full border-b">
+    <header className="sticky top-0 z-40 w-full border-b bg-background">
       <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
         <MainNav items={siteConfig.mainNav} />
         <div className="flex flex-1 items-center justify-end space-x-4">
           <nav className="flex items-center space-x-1">
+            {/* command */}
+            <Combobox />
             <Link
               href={siteConfig.links.github}
               target="_blank"
