4f4181efa75ac6284628b246f43d86902ccf30c3
Michael0520 <po2319987@gmail.com>
2023-09-09 04:40:32 +0800

    feat: 🎸 create props to passing breadcrumb component

--- app/(lobby)/page.tsx
+++ app/(lobby)/page.tsx
@@ -1,10 +1,15 @@
 import Breadcrumb from "./components/Breadcrumb"
 
 const IndexPage = () => {
+  const breadCrumbList = [
+    { name: "台灣", link: "#" },
+    { name: "台北市", link: "#" },
+    { name: "內湖區", link: "", current: true },
+  ]
   return (
     <div>
       {/* Breadcrumb - Block */}
-      <Breadcrumb />
+      <Breadcrumb items={breadCrumbList} />
       {/* Banner - Block */}
 
       {/* Page Title - Block */}
