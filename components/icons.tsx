c3ee236922631c373780f6cdcd9dd1b132d858f0
Michael0520 <po2319987@gmail.com>
2023-09-11 01:16:24 +0800

    feat: 🎸 add chevron left & right icon

--- components/icons.tsx
+++ components/icons.tsx
@@ -1,4 +1,6 @@
 import {
+  ChevronLeft,
+  ChevronRight,
   Image,
   LucideProps,
   Moon,
@@ -14,6 +16,8 @@ export const Icons = {
   moon: Moon,
   search: Search,
   placeholder: Image,
+  chevronLeft: ChevronLeft,
+  chevronRight: ChevronRight,
 
   logo: (props: LucideProps) => (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props}>
