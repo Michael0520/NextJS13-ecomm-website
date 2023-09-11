58f23cdfef905d387ea1346b38ac5b533c8d20cf
Michael0520 <po2319987@gmail.com>
2023-09-12 05:12:53 +0800

    feat: 🎸 add redux-provider in providers component

--- components/providers.tsx
+++ components/providers.tsx
@@ -3,13 +3,19 @@
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 import { type ThemeProviderProps } from "next-themes/dist/types"
+/* Core */
+import { Provider } from "react-redux"
 
+import { reduxStore } from "@/lib/redux"
+/* Instruments */
 import { TooltipProvider } from "@/components/ui/tooltip"
 
 export function Providers({ children, ...props }: ThemeProviderProps) {
   return (
     <NextThemesProvider {...props}>
-      <TooltipProvider>{children}</TooltipProvider>
+      <Provider store={reduxStore} {...props}>
+        <TooltipProvider>{children}</TooltipProvider>
+      </Provider>
     </NextThemesProvider>
   )
 }
