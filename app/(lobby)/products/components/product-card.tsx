3be8d654630db8c0f90a9899ef197676b1425960
Michael0520 <po2319987@gmail.com>
2023-09-13 02:43:38 +0800

    refactor: 💡 product card list structure

--- app/(lobby)/products/components/product-card.tsx
+++ app/(lobby)/products/components/product-card.tsx
@@ -30,13 +30,27 @@ enum PriceStyles {
   SoldOut = "line-through text-gray-400",
 }
 
-// 根據卡片類型渲染對應的標籤
 const renderBadge = (type: CardType) => {
   if (type === CardType.Hot) {
     return (
-      <span className="z-30 rounded-full bg-red-500 px-2 py-1 text-white">
-        熱售中
-      </span>
+      <div className="absolute right-0 top-0 z-30 m-2">
+        {type === CardType.Hot && (
+          <span className="rounded-full bg-red-500 px-2 py-1 text-white">
+            熱售中
+          </span>
+        )}
+      </div>
+    )
+  }
+  return null
+}
+
+const soldOutCover = (type: CardType) => {
+  if (type === CardType.SoldOut) {
+    return (
+      <div className="absolute inset-0 z-20 flex items-center justify-center bg-black/80">
+        <span className="text-4xl font-bold text-white">售完</span>
+      </div>
     )
   }
   return null
@@ -53,34 +67,36 @@ const ProductCard: React.FC<ProductCardProps> = ({
     "https://images.unsplash.com/photo-1455619452474-d2be8b1e70cd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
 
   return (
-    <Card>
+    <Card className="h-full transition duration-500 hover:z-10 hover:scale-105 hover:shadow-2xl">
       <div className="relative">
-        <div className="absolute right-0 top-0 z-30 m-2">
-          {renderBadge(type)}
+        {/* Hot Badge */}
+        {renderBadge(type)}
+
+        {/* Image */}
+        <div className="relative z-10 h-48 w-full">
+          <Image
+            src={imageURL}
+            alt={title || "Image"}
+            layout="fill"
+            objectFit="cover"
+            objectPosition="center"
+          />
+          {/* Sold Out Cover */}
+          {soldOutCover(type)}
         </div>
 
-        {type === CardType.SoldOut && (
-          <div className="absolute inset-0 z-20 flex items-center justify-center bg-black/50">
-            <span className="text-4xl font-bold text-white">售完</span>
-          </div>
-        )}
-
-        <CardHeader>
-          <div className="relative z-10 h-48 w-full">
-            <Image
-              src={imageURL}
-              alt={title || "Image"}
-              layout="fill"
-              objectFit="cover"
-              objectPosition="center"
-            />
-          </div>
-          <CardTitle className="py-2">{title || "卡片標題"}</CardTitle>
+        {/* Title & Description */}
+        <CardHeader className="py-2">
+          <CardTitle>{title || "卡片標題"}</CardTitle>
           <CardDescription>{description || "卡片描述"}</CardDescription>
         </CardHeader>
       </div>
+
+      {/* Content & Footer */}
       <CardContent className="py-2">
-        <span className={PriceStyles[type]}>商品價格：${content || "99"}</span>
+        <span className={`${PriceStyles[type]}`}>
+          商品價格：${content || "99"}
+        </span>
       </CardContent>
     </Card>
   )
