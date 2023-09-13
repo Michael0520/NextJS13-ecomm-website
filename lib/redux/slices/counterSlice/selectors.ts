0a4fe57165e09a5ce0beed352357a87ea4bbf494
Michael0520 <po2319987@gmail.com>
2023-09-14 04:13:33 +0800

    feat: 🎸 update redux state about cart items

--- lib/redux/slices/counterSlice/selectors.ts
+++ lib/redux/slices/counterSlice/selectors.ts
@@ -4,4 +4,13 @@ import type { ReduxState } from '@/lib/redux'
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
 // in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
-export const selectCount = (state: ReduxState) => state.counter.value
+
+export const selectCartState = (state: ReduxState) => state.cart
+
+export const selectCartItems = (state: ReduxState) => state.cart.items
+
+export const selectCartStatus = (state: ReduxState) => state.cart.status
+
+export const selectTotalItemsInCart = (state: ReduxState) => {
+    return state.cart.items.reduce((acc, item) => acc + item.quantity, 0);
+}
