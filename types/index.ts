62d51e8f8067ecf0c7a56d89c08aea8e8aa6d1c1
Michael0520 <po2319987@gmail.com>
2023-09-14 04:08:08 +0800

    feat: 🎸 update cart schema

--- types/index.ts
+++ types/index.ts
@@ -1,5 +1,10 @@
+import { cartItemSchema } from "@/lib/validations/cart"
+import { z } from "zod"
+
 export interface StoredFile {
     id: string
     name: string
     url: string
 }
+
+export type CartLineItem = z.infer<typeof cartItemSchema>
