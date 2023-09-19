81eca7011f667ae98d69a32ccd3e691c714f3f2b
Michael0520 <po2319987@gmail.com>
2023-09-20 05:54:01 +0800

    feat: 🎸 add container

--- app/(lobby)/page.tsx
+++ app/(lobby)/page.tsx
@@ -1,12 +1,14 @@
+import { Shell } from "@/components/shells/shell"
+
 import HeroSection from "./components/hero-section"
 import StoreList from "./components/store-list"
 
 const LobbyIndexPage = () => {
   return (
-    <>
+    <Shell>
       <HeroSection />
       <StoreList />
-    </>
+    </Shell>
   )
 }
 
