470b9c4bfce1e3255db9ff70a4eb3465eabf8590
Michael0520 <po2319987@gmail.com>
2023-09-20 05:54:20 +0800

    feat: 🎸 update storeList page ui

--- app/(lobby)/stores/page.tsx
+++ app/(lobby)/stores/page.tsx
@@ -1,10 +1,17 @@
+import { Button } from "@/components/ui/button"
+import { Shell } from "@/components/shells/shell"
+
 import StoreList from "../components/store-list"
 
 const StoreListPage = () => {
   return (
-    <div>
+    <Shell>
+      <div className="flex gap-4">
+        <Button>Filter</Button>
+        <Button>Sort</Button>
+      </div>
       <StoreList />
-    </div>
+    </Shell>
   )
 }
 
