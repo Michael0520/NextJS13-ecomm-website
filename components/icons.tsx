197511d2240dbef3220aa3ae82cf7835a0f04688
Michael0520 <po2319987@gmail.com>
2023-09-22 02:33:36 +0800

    feat: 🎸 add more icon

--- components/icons.tsx
+++ components/icons.tsx
@@ -15,6 +15,7 @@ import {
   Search,
   ShoppingBag,
   ShoppingCart,
+  Star,
   SunMedium,
   ThumbsUp,
   Trash,
@@ -46,6 +47,7 @@ export const Icons = {
   warning: AlertTriangle,
   trash: Trash,
   UserCircle: UserCircle2,
+  star: Star,
 
   logo: (props: LucideProps) => (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props}>
