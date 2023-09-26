f17dae33c2039acdb0103d77a29066e7059b4911
Michael0520 <po2319987@gmail.com>
2023-09-27 05:08:54 +0800

    feat: 🎸 add login-button & signin-form component

---
+++ components/forms/signin-form.tsx
@@ -0,0 +1,83 @@
+"use client"
+
+import * as React from "react"
+import { signIn } from "next-auth/react"
+
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Icons } from "@/components/icons"
+
+interface SignInFormProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+const SignInForm = ({ className, ...props }: SignInFormProps) => {
+  const [isLoading, setIsLoading] = React.useState<boolean>(false)
+
+  const handleClick = () => {
+    signIn("google")
+  }
+  const onSubmit = async (event: React.SyntheticEvent) => {
+    event.preventDefault()
+    setIsLoading(true)
+
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 3000)
+  }
+
+  return (
+    <div className={cn("grid gap-6", className)} {...props}>
+      <form onSubmit={onSubmit}>
+        <div className="grid gap-2">
+          <div className="grid gap-1">
+            <Label className="sr-only" htmlFor="email">
+              Email
+            </Label>
+            <Input
+              id="email"
+              placeholder="name@example.com"
+              type="email"
+              autoCapitalize="none"
+              autoComplete="email"
+              autoCorrect="off"
+              disabled={isLoading}
+            />
+          </div>
+          <Button disabled={isLoading}>
+            {isLoading && (
+              <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+            )}
+            Sign In with Email
+          </Button>
+        </div>
+      </form>
+      <div className="relative">
+        <div className="absolute inset-0 flex items-center">
+          <span className="w-full border-t" />
+        </div>
+        <div className="relative flex justify-center text-xs uppercase">
+          <span className="bg-background px-2 text-muted-foreground">
+            Or continue with
+          </span>
+        </div>
+      </div>
+      <Button
+        variant="outline"
+        type="button"
+        disabled={isLoading}
+        onClick={handleClick}
+      >
+        {isLoading ? (
+          <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+        ) : (
+          <Icons.gitHub className="mr-2 h-4 w-4" />
+        )}{" "}
+        {/* Github */}
+        Sign In with Google
+      </Button>
+    </div>
+  )
+}
+
+export default SignInForm
