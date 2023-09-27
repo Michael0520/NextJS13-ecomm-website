f91a5680c50417d2f21a09d7c234d6af42cb1513
Michael0520 <po2319987@gmail.com>
2023-09-28 04:42:50 +0800

    feat: 🎸 remove unuse tyle

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -6,9 +6,7 @@ import { useSession } from "next-auth/react"
 import SignInForm from "@/components/forms/signin-form"
 import { Shell } from "@/components/shells/shell"
 
-interface SignInPageProps extends React.HTMLAttributes<HTMLDivElement> {}
-
-const SignInPage = ({ className, ...props }: SignInPageProps) => {
+const SignInPage = () => {
   const { data: session } = useSession()
   if (session) redirect("/")
 
